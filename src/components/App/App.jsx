253d2081aea1614e5529f9cb1c25e6fa2a6e2aaf
Sergey Zhestovsky <Sergey.play123@gmail.com>
2020-12-04 00:41:40 +0200

    append App component

---
+++ src/components/App/App.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { BrowserRouter, Redirect, Route, Switch } from 'react-router-dom';
+import { StylesProvider, MuiThemeProvider } from '@material-ui/core/styles';
+import Notifications from '../Notifications/Notifications';
+
+import Survey from '../pages/Survey/Survey';
+import History from '../pages/History/History';
+import Page from '../parts/Page/Page';
+
+import defaultTheme from '../../assets/mui/default';
+
+const App = () => {
+  return (
+    <BrowserRouter basename="/">
+      <StylesProvider injectFirst>
+        <MuiThemeProvider theme={defaultTheme}>
+          <Page>
+            <Switch>
+              <Route path="/survey" component={Survey} />
+              <Route path="/history" component={History} />
+              <Redirect to="/survey" />
+            </Switch>
+          </Page>
+          <Notifications />
+        </MuiThemeProvider>
+      </StylesProvider>
+    </BrowserRouter>
+  );
+};
+
+export default App;
